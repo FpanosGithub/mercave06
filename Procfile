ac871462bafeffede3d2589bd83162e71bbbfd7c
fpanos <fpanos@triarail.com>
2022-05-05 20:21:07 +0200

    6

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn config.wsgi --log_file -
+web: gunicorn config.wsgi --log-file -
--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default= False)
 
-ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1', 'localhost', '*']
 
 # Application definition
 
@@ -94,7 +94,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 #    }
 #}
 DATABASES = {
-            'default': env.dj_db_url("DATABASE_URL", default ="postgres://postgres@db/postgres")
+            'default': env.dj_db_url("DATABASE_URL")
             }
 
 
