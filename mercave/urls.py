e0e9372ef604c0fcb677e8804453128f80f3624a
fpanos <fpanos@triarail.com>
2022-05-05 22:18:07 +0200

    7

--- mercave/urls.py
+++ mercave/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import HomePage, VistaEjes, VistaCambiadores, VistaEje, VistaCambiador
 
 urlpatterns = [
-    path("", HomePage,name ='home'),
+    path("", VistaEjes, name ='ejes_expl'),
     path("ejes/", VistaEjes, name ='ejes_expl'),
     path("cambiadores/", VistaCambiadores, name ='cambiadores_expl'),
     path("eje/<int:pk>/", VistaEje, name ='ficha_eje'),
